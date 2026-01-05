98a9f97b0d1ac7b67dcda27853f1080d177bf0f5
Gopal <132466447+pchubrdp@users.noreply.github.com>
2026-01-05 15:03:41 +0530

    Added via index.html

---
+++ data.js
@@ -0,0 +1,16 @@
+const campaigns = [
+
+    {
+            id: "airtel",
+        title: "Airtel Payment Bank A/c open ",
+        reward: "120",
+        icon: "airtel.jpg",
+        offerUrl: "https://i.airtel.in/refandearnGNQ4MUCKYC",
+        desc: "Create your zero balance saving bank in Airtel payment bank and get cashback ₹120 within 24 hours",
+        steps: [
+            { icon: "📱", title:"Start and Install", desc: "click on start and install the app from Play Store" },
+            { icon: "🆔", title: "Login the App", desc: "open the app and login using mobile number" },
+            { icon: "💰", title: "open bank and take Screenshot", desc: "open the Airtel payment bank account and take the screenshot as a proof." }
+        ]
+    }
+];
